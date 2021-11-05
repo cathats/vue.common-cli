90389ed57900dd3758bfff6317bd5a53940a5a47
cathats <318009002@qq,com>
2021-11-05 10:46:29 +0800

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import path from 'path'
 import { createVitePlugins } from './build/vite/plugin/index'
 import { resolveEnv } from './build/vite/utils'
